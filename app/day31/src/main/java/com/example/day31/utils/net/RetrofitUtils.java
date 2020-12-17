845ba6d7c5a1900876d6c8872374f2e0295b4daf
guopeng00 <2475050397@qq.com>
2020-12-17 20:31:26 +0800

    jsflsf

---
+++ app/day31/src/main/java/com/example/day31/utils/net/RetrofitUtils.java
@@ -0,0 +1,88 @@
+package com.example.day31.utils.net;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitUtils implements INteWorkInterface {
+    private static RetrofitUtils retrofitUtils;
+    private ApiService apiService;
+    public RetrofitUtils(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URLConstant.Base)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiService = retrofit.create(ApiService.class);
+    }
+    public static RetrofitUtils getRetrofitUtils(){
+        if(retrofitUtils==null){
+            synchronized (RetrofitUtils.class){
+                if(retrofitUtils==null){
+                    retrofitUtils=new RetrofitUtils();
+                }
+            }
+        }
+        return retrofitUtils;
+    }
+
+    @Override
+    public <T> void get(String url, INteCallBack<T> callBack) {
+        apiService.get(url)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull ResponseBody responseBody) {
+                        try {
+                            String string = responseBody.string();
+                            Type[] genericInterfaces = callBack.getClass().getGenericInterfaces();
+                            Type[] actualTypeArguments = ((ParameterizedType) genericInterfaces[0]).getActualTypeArguments();
+                            Type type=actualTypeArguments[0];
+                            T json = new Gson().fromJson(string, type);
+                            callBack.onOk(json);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    @Override
+    public <T> void post(String url, INteCallBack<T> callBack) {
+
+    }
+
+    @Override
+    public <T> void post(String url, HashMap<String, String> map, INteCallBack<T> callBack) {
+
+    }
+}
