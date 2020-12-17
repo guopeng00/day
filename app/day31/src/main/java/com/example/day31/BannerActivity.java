79b5f7067577b06a5b580eff6442b2dfa4c51167
guopeng00 <2475050397@qq.com>
2020-12-17 20:03:50 +0800

    yiiyii

--- app/day31/src/main/java/com/example/day31/BannerActivity.java
+++ app/day31/src/main/java/com/example/day31/BannerActivity.java
@@ -1,14 +1,23 @@
 package com.example.day31;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.youth.banner.Banner;
+
 public class BannerActivity extends AppCompatActivity {
+
+    private Banner myBanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banner);
+        initView();
+    }
+
+    private void initView() {
+        myBanner = (Banner) findViewById(R.id.my_banner);
     }
 }
